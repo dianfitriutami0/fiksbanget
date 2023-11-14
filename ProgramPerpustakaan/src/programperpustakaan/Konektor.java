087105e61ec4c3c67605b612d25284244cfc5e48
dianfitrii <dianutamiii00@gmail.com>
2023-11-14 20:57:28 +0700

    database

--- ProgramPerpustakaan/src/programperpustakaan/Konektor.java
+++ ProgramPerpustakaan/src/programperpustakaan/Konektor.java
@@ -3,30 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package programperpustakaan;
-import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-/**
+import javax.swing.JOptionPane;/**
  *
  * @author lenovo
  */
 public class Konektor {
-     private static Connection MySQLConfig;
-    
-    public static Connection configDB()throws SQLException{
+    private static Connection mysqlconfig;
+
+    public static Connection koneksi() {
         try {
-            String url = "jdbc:mysql://localhost:3306/assetperpus";
+            String url = "jdbc:mysql://localhost:3306/perpustakaan";
             String user = "root";
             String pass = "";
             
-            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            MySQLConfig = DriveManager.getConnection(url, user, pass);
- 
-        } catch(SQLException e){
-            System.out.println("Koneksi Database Gagal" + e.getMessage());
+            mysqlconfig = DriverManager.getConnection(url, user, pass);
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Tidak dapat terhubung ke Database: " + e.getMessage());
+        } finally {
+            // Tutup koneksi di sini jika diperlukan
+            // Cek jika mysqlconfig bukan null sebelum mencoba menutup
+            try {
+                if (mysqlconfig != null) {
+                    mysqlconfig.close();
+                }
+            } catch (SQLException e) {
+            }
         }
-        return MysqlConfig;     
+        return mysqlconfig;
     }
 }
